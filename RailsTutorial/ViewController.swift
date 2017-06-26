c162361eb47dbb311d3dbd5c51ed803ffe6edfe4
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2017-06-26 23:39:49 +0900

    Create Xcode project

---
+++ RailsTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RailsTutorial
+//
+//  Created by Yamazaki Mitsuyoshi on 2017/06/26.
+//  Copyright © 2017 Mitsuyoshi Yamazaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
